4b0dd1fa82fbba503fbdf558bd2cc2cbfa1daee4
Caxivitual <destinyhero789@gmail.com>
2026-07-08 10:16:31 +0700

    [backend - add] IDbContextFactory dependency injection in AuthenticateController

--- src/backend/Conflux.WebApi/Controllers/AuthenticateController.cs
+++ src/backend/Conflux.WebApi/Controllers/AuthenticateController.cs
@@ -1,10 +1,18 @@
+using Conflux.Domain;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Conflux.Controllers;
 
 [ApiController]
 [Route("api/auth")]
 public class AuthenticateController : ControllerBase {
+    private readonly IDbContextFactory<ApplicationDbContext> _dbContextFactory;
+
+    public AuthenticateController(IDbContextFactory<ApplicationDbContext> dbContextFactory) {
+        _dbContextFactory = dbContextFactory;
+    }
+    
     [HttpGet("health", Name = "Healthcheck")]
     public ActionResult Healthcheck() {
         return Ok();
