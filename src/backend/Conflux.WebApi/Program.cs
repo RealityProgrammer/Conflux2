9eaa981dd5cec1d9067f4a657f42a6cacf407938
Caxivitual <destinyhero789@gmail.com>
2026-07-08 11:55:53 +0700

    [backend - add] JWT authentication services

--- src/backend/Conflux.WebApi/Program.cs
+++ src/backend/Conflux.WebApi/Program.cs
@@ -1,19 +1,41 @@
 using Conflux.Domain;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Authenticate.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => {
+    options.TokenValidationParameters = new() {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)),
+    };
+});
+
+builder.Services.AddAuthorization();
+
+// API related services.
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
-// Add database services.
+// Database related services.
 builder.Services.AddDbContextFactory<ApplicationDbContext>(options => {
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"), options => {
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Database"), options => {
         options.MigrationsAssembly("Conflux.Infrastructure");
     });
 });
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 // Configure OpenAPI and Swagger for development environment.
 if (app.Environment.IsDevelopment()) {
