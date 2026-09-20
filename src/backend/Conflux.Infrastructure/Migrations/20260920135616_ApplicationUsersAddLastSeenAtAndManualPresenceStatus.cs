4a94d668bb3215b12dfbe12fb5446af1ecc05454
Caxivitual <destinyhero789@gmail.com>
2026-09-20 20:59:11 +0700

    [backend - add] missing migration of ApplicationUser LastSeenAt and ManualPresenceStatus

---
+++ src/backend/Conflux.Infrastructure/Migrations/20260920135616_ApplicationUsersAddLastSeenAtAndManualPresenceStatus.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Conflux.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApplicationUsersAddLastSeenAtAndManualPresenceStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastSeenAt",
+                table: "AspNetUsers",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<int>(
+                name: "ManualPresenceStatus",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastSeenAt",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ManualPresenceStatus",
+                table: "AspNetUsers");
+        }
+    }
+}
