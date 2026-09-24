4f32a4261eac92f1fc498a2e34484e009a3b9c78
Caxivitual <destinyhero789@gmail.com>
2026-09-25 06:56:54 +0700

    [backend - refactor] remove default initialized value in UserServiceOptions.cs

--- src/backend/Conflux.Application/Options/UserServiceOptions.cs
+++ src/backend/Conflux.Application/Options/UserServiceOptions.cs
@@ -1,6 +1,6 @@
 namespace Conflux.Application.Options;
 
 public class UserServiceOptions {
-    public long MaxAvatarSizeBytes { get; set; } = 1048576;
-    public long MaxBannerSizeBytes { get; set; } = 5242880;
+    public long MaxAvatarSizeBytes { get; set; }
+    public long MaxBannerSizeBytes { get; set; }
 }
