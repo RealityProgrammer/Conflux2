649f762c6e7251c78bbc574f1926cb9781f437cf
Caxivitual <destinyhero789@gmail.com>
2026-09-24 20:46:45 +0700

    [frontend - add] Logout button in SettingsLayout

--- src/frontend/src/pages/settings/SettingsLayout.tsx
+++ src/frontend/src/pages/settings/SettingsLayout.tsx
@@ -1,18 +1,34 @@
 import {Separator} from "radix-ui";
 import {NavLink, Outlet} from "react-router";
 import type {ReactNode} from "react";
+import {useAuthorization} from "../../contexts/AuthContext.tsx";
 
 export default function SettingsLayout() {
+  const auth = useAuthorization();
+
   return (
     <div className="w-dvw h-dvh grid grid-cols-5 text-white">
       <aside className="col-span-1 bg-gray-725 border-r-2 border-r-gray-600 p-2 overflow-y-auto scrollbar-none">
-        <p className="group-label">Group 1</p>
+        <p className="group-label">Account</p>
 
         <div className="flex flex-col gap-1">
           <NavigateLink to="profile">Profile</NavigateLink>
         </div>
 
         <Separator.Root decorative className="flex-1 horizontal-separator my-3"/>
+
+        <p className="group-label text-red-500!">Danger</p>
+
+        <div className="border-2 border-red-800 bg-red-500/20 rounded-lg">
+          <button
+            className="w-full px-3 py-1 text-left cursor-pointer transition-colors text-red font-bold hover-highlight"
+            onClick={() => {
+              auth.logout();
+            }}
+          >
+            Logout
+          </button>
+        </div>
       </aside>
 
       <section className="col-span-4 bg-gray-675 overflow-y-auto scrollbar-none p-2">
@@ -28,7 +44,7 @@ function NavigateLink({to, children}: {to: string, children: ReactNode}) {
       to={`/settings/${encodeURIComponent(to)}`}
       className={({ isActive }) =>
         `px-3 py-1 rounded-full text-left cursor-pointer transition-colors ${
-          isActive ? "bg-gray-600 text-white" : "hover-highlight text-gray-300"
+          isActive ? "text-white" : "hover-highlight text-gray-300"
         }`
       }
     >
